74828ef88c7c2a5a595f4979df79592eeb522f96
Ebony <ebony.williams@gmail.com>
2021-05-14 13:00:44 -0400

    updates folders

--- article-preview-component-master/script/app.js
+++
@@ -1,7 +0,0 @@
-window.onload = function () {
-    function myFunction() {
-        let element = document.getElementById("share");
-        let panel = document.getElementById("panel");
-        panel.classList.toggle("show");
-     }
-    }
